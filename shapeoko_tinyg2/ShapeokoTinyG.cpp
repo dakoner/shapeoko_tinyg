a009bb10a849f3b84dd3a73d1861bd60480b4733
David Konerding <dek@google.com>
2015-03-01 06:26:15 -0800

    Keep adding more code.

--- shapeoko_tinyg2/ShapeokoTinyG.cpp
+++ shapeoko_tinyg2/ShapeokoTinyG.cpp
@@ -6,8 +6,8 @@
 // DESCRIPTION:   The example implementation of the demo camera.
 //                Simulates generic digital camera and associated automated
 //                microscope devices and enables testing of the rest of the
-//                system without the need to connect to the actual hardware. 
-//                
+//                system without the need to connect to the actual hardware.
+//
 // AUTHOR:        Nenad Amodaj, nenad@amodaj.com, 06/08/2005
 //
 // COPYRIGHT:     University of California, San Francisco, 2006
@@ -32,24 +32,12 @@
 #include <iostream>
 
 
-
 using namespace std;
 
 // External names used used by the rest of the system
 // to load particular device from the "ShapeokoTinyGCamera.dll" library
 const char* g_XYStageDeviceName = "DXYStage";
 const char* g_HubDeviceName = "DHub";
-
-// constants for naming pixel types (allowed values of the "PixelType" property)
-const char* g_PixelType_8bit = "8bit";
-const char* g_PixelType_16bit = "16bit";
-const char* g_PixelType_32bitRGB = "32bitRGB";
-const char* g_PixelType_64bitRGB = "64bitRGB";
-const char* g_PixelType_32bit = "32bit";  // floating point greyscale
-
-// constants for naming camera modes
-const char* g_Sine_Wave = "Artificial Waves";
-const char* g_Norm_Noise = "Noise";
 
 ///////////////////////////////////////////////////////////////////////////////
 // Exported MMDevice API
@@ -89,7 +77,7 @@ MODULE_API void DeleteDevice(MM::Device* pDevice)
 // CShapeokoTinyGXYStage implementation
 // ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-CShapeokoTinyGXYStage::CShapeokoTinyGXYStage() : 
+CShapeokoTinyGXYStage::CShapeokoTinyGXYStage() :
 CXYStageBase<CShapeokoTinyGXYStage>(),
 stepSize_um_(0.015),
 posX_um_(0.0),
@@ -221,15 +209,23 @@ int CShapeokoTinyGXYStage::SetRelativePositionSteps(long x, long y)
 // none implemented
 
 
+
+ShapeokoTinyGHub::ShapeokoTinyGHub():
+      initialized_(false),
+      busy_(false)
+{
+  CPropertyAction* pAct  = new CPropertyAction(this, &ShapeokoTinyGHub::OnPort);
+  CreateProperty(MM::g_Keyword_Port, "Undefined", MM::String, false, pAct, true);
+}
+
 int ShapeokoTinyGHub::Initialize()
 {
   	initialized_ = true;
- 
 	return DEVICE_OK;
 }
 
 int ShapeokoTinyGHub::DetectInstalledDevices()
-{  
+{
    ClearInstalledDevices();
 
    // make sure this method is called before we look for available devices
@@ -238,7 +234,7 @@ int ShapeokoTinyGHub::DetectInstalledDevices()
    char hubName[MM::MaxStrLength];
    GetName(hubName); // this device name
    for (unsigned i=0; i<GetNumberOfDevices(); i++)
-   { 
+   {
       char deviceName[MM::MaxStrLength];
       bool success = GetDeviceName(i, deviceName, MM::MaxStrLength);
       if (success && (strcmp(hubName, deviceName) != 0))
@@ -247,10 +243,304 @@ int ShapeokoTinyGHub::DetectInstalledDevices()
          AddInstalledDevice(pDev);
       }
    }
-   return DEVICE_OK; 
+   return DEVICE_OK;
 }
 
 void ShapeokoTinyGHub::GetName(char* pName) const
 {
    CDeviceUtils::CopyLimitedString(pName, g_HubDeviceName);
+}
+
+int ShapeokoTinyGHub::OnPort(MM::PropertyBase* pProp, MM::ActionType pAct)
+{
+   if (pAct == MM::BeforeGet)
+   {
+      pProp->Set(port_.c_str());
+   }
+   else if (pAct == MM::AfterSet)
+   {
+      pProp->Get(port_);
+      portAvailable_ = true;
+   }
+   return DEVICE_OK;
+}
+
+int ShapeokoTinyGHub::OnCommand(MM::PropertyBase* pProp, MM::ActionType pAct)
+{
+   if (pAct == MM::BeforeGet)
+   {
+	   pProp->Set(commandResult_.c_str());
+   }
+   else if (pAct == MM::AfterSet)
+   {
+	   std::string cmd;
+      pProp->Get(cmd);
+	  if(cmd.compare(commandResult_) ==0)  // command result still there
+		  return DEVICE_OK;
+	  int ret = SendCommand(cmd,commandResult_);
+	  if(DEVICE_OK != ret){
+		  commandResult_.assign("Error!");
+		  return DEVICE_ERR;
+	  }
+   }
+   return DEVICE_OK;
+}
+
+int ShapeokoTinyGHub::SendCommand(std::string command, std::string &returnString)
+{
+  LogMessage("SendCommand");
+  LogMessage("command=" + command);
+   if(!portAvailable_)
+	   return ERR_NO_PORT_SET;
+   // needs a lock because the other Thread will also use this function
+   MMThreadGuard(this->executeLock_);
+   PurgeComPortH();
+   int ret = DEVICE_OK;
+   SetAnswerTimeoutMs(300.0); //for normal command
+
+   	if(command == "G28.2 X0 Y0")
+        {
+          LogMessage("setting long timeout.");
+           	SetAnswerTimeoutMs(60000.0);
+        }
+
+
+   LogMessage("Write command.");
+   ret = SetCommandComPortH(command.c_str(),"\r\n");
+   LogMessage("set command, ret=" + ret);
+   if (ret != DEVICE_OK)
+   {
+	    LogMessage("command write fail");
+	   return ret;
+   }
+   std::string an;
+   if(command.c_str()[0] == 0x18 ){
+     LogMessage("Send reset.");
+        CDeviceUtils::SleepMs(600);
+            	SetAnswerTimeoutMs(10000.0);
+        	ret = GetSerialAnswerComPortH(an,"\r\n");
+	    // ret = GetParameters();
+	    // returnString.assign("ok");
+	    //     LogMessage(std::string("Reset!"));
+		return DEVICE_OK;
+   }
+   else if(command.c_str()[0] == '$' && command.c_str()[1] == 's' && command.c_str()[2] == 'r'){
+
+     LogMessage("Status request.");
+            	SetAnswerTimeoutMs(10000.0);
+        	ret = GetSerialAnswerComPortH(an,"\r\n");
+           if (ret != DEVICE_OK)
+           {
+        	   LogMessage(std::string("answer get error!"));
+        	  return ret;
+           }
+           returnString.assign(an);
+           return DEVICE_OK;
+
+   }
+   else{
+     LogMessage("Other.");
+	   try
+	   {
+
+			ret = GetSerialAnswerComPortH(an,"\r\n");
+		   //ret = comPort->Read(answer,3,charsRead);
+		   if (ret != DEVICE_OK)
+		   {
+			   LogMessage(std::string("answer get error!_"));
+			  return ret;
+		   }
+                   LogMessage("answer:");
+		   LogMessage(std::string(an));
+		   //sample:>>? >><Idle,MPos:0.000,0.000,0.000,WPos:0.000,0.000,0.000>
+		   if (an.length() <1)
+			  return DEVICE_ERR;
+		   returnString.assign(an);
+ 		   // if (returnString.find("ok") != std::string::npos)
+			   return DEVICE_OK;
+		   // else
+		   //         return DEVICE_ERR;
+	   }
+	   catch(...)
+	   {
+		  LogMessage("Exception in send command!");
+		  return DEVICE_ERR;
+	   }
+
+   }
+}
+
+MM::DeviceDetectionStatus ShapeokoTinyGHub::DetectDevice(void)
+{
+  LogMessage("DetectDevice");
+  if (initialized_)
+      return MM::CanCommunicate;
+
+   // all conditions must be satisfied...
+   MM::DeviceDetectionStatus result = MM::Misconfigured;
+   char answerTO[MM::MaxStrLength];
+
+   try
+   {
+      std::string portLowerCase = port_;
+      for( std::string::iterator its = portLowerCase.begin(); its != portLowerCase.end(); ++its)
+      {
+         *its = (char)tolower(*its);
+      }
+      if( 0< portLowerCase.length() &&  0 != portLowerCase.compare("undefined")  && 0 != portLowerCase.compare("unknown") )
+      {
+         result = MM::CanNotCommunicate;
+         // record the default answer time out
+         GetCoreCallback()->GetDeviceProperty(port_.c_str(), "AnswerTimeout", answerTO);
+
+         // device specific default communication parameters
+         // for Arduino Duemilanova
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_Handshaking, "Off");
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_BaudRate, "115200" );
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_StopBits, "1");
+         // Arduino timed out in GetControllerVersion even if AnswerTimeout  = 300 ms
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), "AnswerTimeout", "500.0");
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), "DelayBetweenCharsMs", "0");
+         MM::Device* pS = GetCoreCallback()->GetDevice(this, port_.c_str());
+         pS->Initialize();
+         // The first second or so after opening the serial port, the Arduino is waiting for firmwareupgrades.  Simply sleep 1 second.
+         CDeviceUtils::SleepMs(2000);
+         MMThreadGuard myLock(executeLock_);
+         PurgeComPort(port_.c_str());
+         int ret = GetStatus();
+         // later, Initialize will explicitly check the version #
+         if( DEVICE_OK != ret )
+         {
+           LogMessage("Got:");
+            LogMessageCode(ret,true);
+         }
+         else
+         {
+            // to succeed must reach here....
+            result = MM::CanCommunicate;
+         }
+         pS->Shutdown();
+         // always restore the AnswerTimeout to the default
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), "AnswerTimeout", answerTO);
+
+      }
+   }
+   catch(...)
+   {
+      LogMessage("Exception in DetectDevice!",false);
+   }
+
+   return result;
+}
+
+int ShapeokoTinyGHub::SetAnswerTimeoutMs(double timeout)
+{
+      if(!portAvailable_)
+	   return ERR_NO_PORT_SET;
+     GetCoreCallback()->SetDeviceProperty(port_.c_str(), "AnswerTimeout",  CDeviceUtils::ConvertToString(timeout));
+   return DEVICE_OK;
+}
+
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
+template <class Type>
+Type stringToNum(const std::string& str)
+{
+	std::istringstream iss(str);
+	Type num;
+	iss >> num;
+	return num;
+}
+
+// private and expects caller to:
+// 1. guard the port
+// 2. purge the port
+int ShapeokoTinyGHub::GetStatus()
+{
+  LogMessage("GetStatus");
+   std::string cmd;
+   cmd.assign("$sr"); // x step/mm
+   std::string returnString;
+   int ret = SendCommand(cmd,returnString);
+   if (ret != DEVICE_OK)
+   {
+         LogMessage("command send failed!");
+    return ret;
+   }
+   LogMessage("returnString=" + returnString);
+       // X position:          0.000 mm
+       // Y position:          0.000 mm
+       // Z position:          0.000 mm
+       // A position:          0.000 deg
+       // Feed rate:           0.000 mm/min
+       // Velocity:            0.000 mm/min
+       // Units:               G21 - millimeter mode
+       // Coordinate system:   G54 - coordinate system 1
+       // Distance mode:       G90 - absolute distance mode
+       // Feed rate mode:      G94 - units-per-minute mode (i.e. feedrate mode)
+       // Machine state:       Ready
+   std::vector<std::string> tokenInput;
+   //      char* pEnd;
+   	CDeviceUtils::Tokenize(returnString, tokenInput, "\r\n");
+        for(std::vector<std::string>::iterator i = tokenInput.begin(); i != tokenInput.end(); ++i) {
+          LogMessage("Token input: ");
+          LogMessage(*i);
+          string x;
+          if (i->substr(0, 10) == "X position") {
+            x = i->substr(21,10);
+            std::vector<std::string> spl;
+            spl = split(x, ' ');
+            MPos[0] = stringToNum<double>(spl[0]);
+          }
+          if (i->substr(0, 10) == "Y position") {
+            x = i->substr(21,10);
+            std::vector<std::string> spl;
+            spl = split(x, ' ');
+            MPos[1] = stringToNum<double>(spl[0]);
+          }
+          if (i->substr(0, 10) == "Z position") {
+            x = i->substr(21,10);
+            std::vector<std::string> spl;
+            spl = split(x, ' ');
+            MPos[2] = stringToNum<double>(spl[0]);
+          }
+          if (i->substr(0, 9) == "Velocity:") {
+            x = i->substr(21,10);
+          }
+          if (i->substr(0, 6) == "Units:") {
+            // TODO(dek): correct these if wrong.
+            x = i->substr(21,10);
+          }
+          if (i->substr(0, 18) == "Coordinate system:") {
+            x = i->substr(21,10);
+          }
+          if (i->substr(0, 14) == "Distance mode:") {
+            // TODO(dek): correct these if wrong.
+            x = i->substr(21,10);
+          }
+          if (i->substr(0, 14) == "Machine state:") {
+            x = i->substr(21,10);
+            SetProperty("Status",x.c_str());
+          }
+          if (!x.empty()) {
+            LogMessage("Parsed line:");
+                LogMessage(x);
+                 }
+
+        }
+        return DEVICE_OK;
 }
--- shapeoko_tinyg2/ShapeokoTinyG.h
+++ shapeoko_tinyg2/ShapeokoTinyG.h
@@ -39,12 +39,23 @@
 //////////////////////////////////////////////////////////////////////////////
 // Error codes
 //
-#define ERR_UNKNOWN_MODE         102
-#define ERR_UNKNOWN_POSITION     103
-#define ERR_IN_SEQUENCE          104
-#define ERR_SEQUENCE_INACTIVE    105
-#define ERR_STAGE_MOVING         106
-#define HUB_NOT_AVAILABLE        107
+// #define ERR_UNKNOWN_MODE         102
+// #define ERR_UNKNOWN_POSITION     103
+// #define ERR_IN_SEQUENCE          104
+// #define ERR_SEQUENCE_INACTIVE    105
+#define ERR_STAGE_MOVING         110
+// #define HUB_NOT_AVAILABLE        107
+
+#define ERR_UNKNOWN_POSITION 101
+#define ERR_INITIALIZE_FAILED 102
+#define ERR_WRITE_FAILED 103
+#define ERR_CLOSE_FAILED 104
+#define ERR_BOARD_NOT_FOUND 105
+#define ERR_PORT_OPEN_FAILED 106
+#define ERR_COMMUNICATION 107
+#define ERR_NO_PORT_SET 108
+#define ERR_VERSION_MISMATCH 109
+
 
 const char* NoHubError = "Parent Hub not defined.";
 
@@ -56,28 +67,54 @@ const char* NoHubError = "Parent Hub not defined.";
 class ShapeokoTinyGHub : public HubBase<ShapeokoTinyGHub>
 {
 public:
-   ShapeokoTinyGHub() :
-      initialized_(false),
-      busy_(false)
-   {}
-   ~ShapeokoTinyGHub() {}
+  ShapeokoTinyGHub();
+  ~ShapeokoTinyGHub() { Shutdown();}
 
    // Device API
    // ---------
    int Initialize();
-   int Shutdown() {return DEVICE_OK;};
+  int Shutdown() {initialized_ = false; return DEVICE_OK;};
    void GetName(char* pName) const; 
    bool Busy() { return busy_;} ;
+
+   // property handlers
+   int OnPort(MM::PropertyBase* pPropt, MM::ActionType eAct);
+   int OnCommand(MM::PropertyBase* pProp, MM::ActionType pAct);
 
    // HUB api
    int DetectInstalledDevices();
 
+   int SendCommand(std::string command, std::string &returnString);
+   int SetAnswerTimeoutMs(double timout);
+   MM::DeviceDetectionStatus DetectDevice(void);
+   int PurgeComPortH() {return PurgeComPort(port_.c_str());}
+   int WriteToComPortH(const unsigned char* command, unsigned len) {return WriteToComPort(port_.c_str(), command, len);}
+   int ReadFromComPortH(unsigned char* answer, unsigned maxLen, unsigned long& bytesRead)
+   {
+      return ReadFromComPort(port_.c_str(), answer, maxLen, bytesRead);
+   }
+   int SetCommandComPortH(const char* command, const char* term)
+   {
+           return SendSerialCommand(port_.c_str(),command,term);
+   }
+    int GetSerialAnswerComPortH (std::string& ans,  const char* term)
+        {
+                return GetSerialAnswer(port_.c_str(),term,ans);
+        }
+   int GetStatus(); 
+
 private:
    void GetPeripheralInventory();
-
    std::vector<std::string> peripherals_;
    bool initialized_;
    bool busy_;
+
+   std::string port_;
+   bool portAvailable_;
+   std::string commandResult_;
+   MMThreadLock executeLock_;
+   double MPos[3];
+   double WPos[3];
 };
 
 
